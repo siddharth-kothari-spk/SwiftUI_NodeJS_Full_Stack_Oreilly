0b7776d603eca47c818d0ee19890b60aa0c415f7
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-03-05 13:32:13 +0530

    added notes.json and acessed same on browser

--- notes-api/src/index.js
+++ notes-api/src/index.js
@@ -1,8 +1,18 @@
 //import express from 'express';
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const fs = require('fs');
 
+const app = express();
+
+app.get('/notes', (req, res) => {
+    fs.readFile(__dirname + '/' + "notes.json", 'utf-8', (err, data) => {
+        if (err) {
+            return console.log(err)
+        }
+        res.send(data)
+    })
+})
 app.listen(3000, () => {
     console.log("Server is up on port 3000")
 })
